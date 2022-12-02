f4b3eb8449a7b74ca21d723f4818a488a3df38d3
wjun <wjunjobs@outlook.com>
2022-12-02 16:33:56 +0800

    👑👑trait

--- generics-trait/src/main.rs
+++ generics-trait/src/main.rs
@@ -1,8 +1,19 @@
+use std::fmt::{Debug, Display, Formatter};
+
 fn main() {
     // let vec1 = vec![1, 2, 3, 4, 5];
     // search_max(&vec1);
     // 实例化泛型结构体
     let point = Point { x: 1, y: 1.1 };
+    // 实现类似多态的概念
+    let people = People {};
+    say(&people);
+    say1(&people);
+    let dog = Dog {};
+    say(&dog);
+    say1(&dog);
+    say2(&people, &dog);
+    say3(&people, &dog);
 }
 
 // 定义泛型方法，当前缺少泛型边界后续调试
@@ -46,3 +57,79 @@ impl Point<i32, i32> {
     }
 }
 // 注：rust的泛型是单态化
+
+// trait 特质，某些特定的类型拥有与其他类型共享的功能，通过 trait 以一种抽象的方式定义共享行为
+// 定义 trait
+trait Animal {
+    fn say(&self);
+
+    fn eat(&self);
+}
+
+// 定义结构体
+#[derive(Debug)]
+struct People {}
+
+#[derive(Debug)]
+struct Dog {}
+
+// 为结构体实现trait
+impl Animal for People {
+    fn say(&self) {
+        println!("我是人")
+    }
+
+    fn eat(&self) {
+        println!("我吃熟的")
+    }
+}
+
+impl Animal for Dog {
+    fn say(&self) {
+        println!("我是狗")
+    }
+
+    fn eat(&self) {
+        println!("我吃生的")
+    }
+}
+
+impl Display for People {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Ok(())
+    }
+}
+
+impl Display for Dog {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Ok(())
+    }
+}
+
+// 一旦结构体实现了某个trait，就可以看成是具体的trait数据类型。例如上述的People和Dog都可以统一看成Animal类型
+fn say(animal: &impl Animal) {
+    animal.eat()
+}
+
+// 对say进行改写
+fn say1<T: Animal>(animal: &T) {
+    animal.eat();
+}
+
+// 对say1在进行一次改写，要求参数不行实现了Animal还要实现Display和Debug，同时有两个参数
+fn say2<T: Animal + Display + Debug, K: Animal + Display>(animal1: &T, animal2: &K) {
+    println!("{:#?}", animal1);
+    // 因为animal2没有实现Debug
+    // println!("{:#?}", animal1);
+    println!("{}", animal2);
+}
+
+// 对say2在进行一次改写，trait bound 这样写太长了，进行简化
+fn say3<T, K>(animal1: &T, animal2: &K)
+    where T: Animal + Display + Debug,
+          K: Animal + Display {
+    println!("{:#?}", animal1);
+    // 因为animal2没有实现Debug
+    // println!("{:#?}", animal1);
+    println!("{}", animal2);
+}
