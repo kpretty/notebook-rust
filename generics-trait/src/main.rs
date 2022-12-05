783dfe9c5bc96a2bc46efcd223ecdce0a8a5156f
wjun <wjunjobs@outlook.com>
2022-12-05 13:36:10 +0800

    👑👑trait

--- generics-trait/src/main.rs
+++ generics-trait/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     // let vec1 = vec![1, 2, 3, 4, 5];
     // search_max(&vec1);
     // 实例化泛型结构体
-    let point = Point { x: 1, y: 1.1 };
+    let _point = Point { x: 1, y: 1.1 };
     // 实现类似多态的概念
     let people = People {};
     say(&people);
@@ -29,12 +29,14 @@ fn main() {
 // }
 
 // 定义泛型结构体
+#[allow(dead_code)]
 struct Point<T, V> {
     x: T,
     y: V,
 }
 
 // 定义泛型枚举
+#[allow(dead_code)]
 enum MyResult<T> {
     Ok(T),
     Err,
@@ -44,13 +46,15 @@ enum MyResult<T> {
 // T V 为结构体的泛型
 impl<T, V> Point<T, V> {
     // T1 V1 为方法的泛型
-    fn distance<T1, V1>(&self, point: &Point<T1, V1>) -> f64 {
+    #[allow(dead_code)]
+    fn distance<T1, V1>(&self, _point: &Point<T1, V1>) -> f64 {
         1.2
     }
 }
 
 // 为具体类型的结构体实现方法'
 impl Point<i32, i32> {
+    #[allow(dead_code)]
     fn distance_i_f(&self, point: &Point<i32, i32>) -> f64 {
         let x = (self.x - point.x).pow(2) + (self.y - point.y).pow(2);
         return x as f64;
@@ -95,13 +99,13 @@ impl Animal for Dog {
 }
 
 impl Display for People {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
         Ok(())
     }
 }
 
 impl Display for Dog {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
         Ok(())
     }
 }
